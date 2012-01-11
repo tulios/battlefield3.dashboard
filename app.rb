433e3a85b13a7faa498e3e6c5cc2b197b07707ca
tulios <tulio@corp.globo.com>
2012-01-11 19:37:06 -0200

    retrieving info

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'rest-client'
 
 get '/' do
+  url_service = "http://battlelog.battlefield.com/bf3/user/overviewBoxStats/<USER_ID>/"
+  # profile-identifer
+  users = {
+    tuliornelas: "2832658994908040861",
+    danielfmartins: "2832660143811904223",
+    brunofmsouza: "2832660143832117465",
+    leosipe: "2832658801630550806"
+  }
+
+  data = users.values.collect do |user_id|
+    RestClient.get(url_service.gsub("<USER_ID>", user_id))
+  end
+
+  puts data
+
   erb :index
 end
