c2255fa03f20658a6a430e6d4d35869238a8ab4d
tulios <ornelas.tulio@gmail.com>
2012-01-13 19:50:16 -0200

    some refactoring

--- app.rb
+++ app.rb
@@ -3,33 +3,44 @@ require 'rest-client'
 require 'json'
 require 'ostruct'
 
-get '/' do
-  url_service = "http://battlelog.battlefield.com/bf3/user/overviewBoxStats/<USER_ID>/"
-  # profile-identifer
-  users = {
-    tuliornelas: "2832658994908040861",
-    danielfmartins: "2832660143811904223",
-    brunofmsouza: "2832660143832117465",
-    leosipe: "2832658801630550806",
-    el_mariel: "2832659598374270655",
-    lurehendi: "2832660143835504606",
-    danilo_moret: "2832659982774753240"
-  }
+URL_SERVICE = "http://battlelog.battlefield.com/bf3/user/overviewBoxStats/<USER_ID>/"
+# profile-identifer
 
-  cdn_url = "http://battlelog-cdn.battlefield.com"
-  picture_path = "/public/profile/kits/s/"
-  dogtag_path = "/public/profile/bf3/stats/dogtags/"
+CDN_URL = "http://battlelog-cdn.battlefield.com"
+PICTURE_PATH = "/public/profile/kits/s/"
+DOGTAG_PATH = "/public/profile/bf3/stats/dogtags/"
 
-  @soldiers = users.values.collect do |user_id|
-    json_data = RestClient.get(url_service.gsub("<USER_ID>", user_id))
+USERS = {
+  tuliornelas: "2832658994908040861",
+  danielfmartins: "2832660143811904223",
+  brunofmsouza: "2832660143832117465",
+  leosipe: "2832658801630550806",
+  el_mariel: "2832659598374270655",
+  lurehendi: "2832660143835504606",
+  danilo_moret: "2832659982774753240"
+}
+
+get '/' do  
+  @soldiers = USERS.values.collect do |user_id|
+    json_data = RestClient.get(URL_SERVICE.gsub("<USER_ID>", user_id))
     hash = JSON(json_data)["data"]["soldiersBox"].first
+    new_soldier(hash)
+  end
 
+  @soldiers.sort_by! {|obj| obj.score}
+  @soldiers.reverse!
+
+  erb :index
+end
+
+helpers do
+  def new_soldier hash
     persona = hash["persona"]
     persona_id = persona["personaId"]
-    picture = persona["picture"] ? "#{cdn_url}#{picture_path}#{persona["picture"]}.png" : "#{cdn_url}#{picture_path}bf3-us-assault.png"
+    picture = persona["picture"] ? "#{CDN_URL}#{PICTURE_PATH}#{persona["picture"]}.png" : "#{CDN_URL}#{PICTURE_PATH}bf3-us-assault.png"
     dogtag = hash["dogtagsForPersona"][persona_id]
-    basic_dogtag = dogtag ? "#{cdn_url}#{dogtag_path}la/t/#{dogtag["basicDogTag"]["image"]}.png" : "#{cdn_url}#{dogtag_path}lb/t/defaulttag_right.png"
-    advanced_dogtag = dogtag ? "#{cdn_url}#{dogtag_path}lb/t/#{dogtag["advancedDogTag"]["image"]}.png" : "#{cdn_url}#{dogtag_path}la/t/defaulttag_right.png"
+    basic_dogtag = dogtag ? "#{CDN_URL}#{DOGTAG_PATH}la/t/#{dogtag["basicDogTag"]["image"]}.png" : "#{CDN_URL}#{DOGTAG_PATH}lb/t/defaulttag_right.png"
+    advanced_dogtag = dogtag ? "#{CDN_URL}#{DOGTAG_PATH}lb/t/#{dogtag["advancedDogTag"]["image"]}.png" : "#{CDN_URL}#{DOGTAG_PATH}la/t/defaulttag_right.png"
 
     OpenStruct.new({
       id: persona_id,
@@ -46,9 +57,4 @@ get '/' do
       kills: hash["kills"]
     })
   end
-
-  @soldiers.sort_by! {|obj| obj.score}
-  @soldiers.reverse!
-
-  erb :index
 end
